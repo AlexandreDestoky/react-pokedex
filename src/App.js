928d20102fd3dfbafac5add37ecde31ee4f0fc60
Destoky <adestoky@gmail.com>
2021-08-31 15:43:28 +0200

    Fetch
    La fonction de fetch avec useEffect fonctionne

--- src/App.js
+++ src/App.js
@@ -3,34 +3,23 @@ import Cardlist from "./Components/Cardlist/Cardlist";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [pokemonList, setPokemonList] = useState(["un","deux"]);
-  useEffect(async () => {
-    const fetchPokemonBase = () => {
-      let listPoke = [];
-      fetch("https://pokeapi.co/api/v2/pokemon/?limit=4")
-        .then(reponse => reponse.json())
-        .then(allPoke => {
-          allPoke.results.forEach(pokemon => {
-            listPoke.push(fetchPokemonComplet(pokemon));
-            // fetchPokemonComplet(pokemon);
-          });
-        });
-      console.log(listPoke);
-      setPokemonList(listPoke);
-    };
-    fetchPokemonBase();
+  const [pokemonList, setPokemonList] = useState([]);
 
-    function fetchPokemonComplet(pokemon) {
-      let url = pokemon.url;
-      return fetch(url)
-        .then(reponse => reponse.json())
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon/?limit=4")
+      .then(reponse => reponse.json())
+      .then(allPoke => allPoke.results.forEach(el => fetchAllPoke(el.url)));
+
+    function fetchAllPoke(pokemons) {
+      fetch(pokemons)
+        .then(response => response.json())
+        .then(data => setPokemonList(pokemonList => [...pokemonList, data]));
     }
   }, []);
 
   return (
     <div className="App">
-      <Cardlist />
-      {pokemonList.map(el => (<p>{el}</p>))}
+      <Cardlist pokemons={pokemonList} />
     </div>
   );
 }
