de2cc5caa51ed168a9fc7571a552b506c4be3977
Destoky <adestoky@gmail.com>
2021-09-14 12:08:44 +0200

    Identifiant et Style
    Amélioration du style et ajout du numéro de chaque pokemon

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -3,8 +3,10 @@ import "./Card.css";
 
 export default function Card(props) {
   const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1);
+
   return (
     <div className={"card-pokemon " + props.pokemon.types[0].type.name}>
+      <span className="numero">#{props.pokemon.id}</span>
       <h2>{props.pokemon.name.toUpperCase()}</h2>
       <p>
         Type :{" "}
